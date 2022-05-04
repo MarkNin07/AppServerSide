a31c6bca921470bbbd484c7b8c10746ca7045cfd
Marcos Nino <tmnino@uninorte.edu.co>
2022-05-04 09:21:02 -0500

    The springboot project was created to start coding our back-end

---
+++ src/main/java/com/sofkau/software/appserversider/AppServerSiderApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.software.appserversider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerSiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerSiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/software/appserversider/AppServerSiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.software.appserversider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerSiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
