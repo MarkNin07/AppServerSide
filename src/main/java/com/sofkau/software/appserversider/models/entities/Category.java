a1342037cf138c2f5676c9afc8a9cfae1c342143
Marcos Nino <tmnino@uninorte.edu.co>
2022-05-05 04:10:50 -0500

    Category
    attributes were created

--- src/main/java/com/sofkau/software/appserversider/models/entities/Category.java
+++ src/main/java/com/sofkau/software/appserversider/models/entities/Category.java
@@ -1,4 +1,23 @@
 package com.sofkau.software.appserversider.models.entities;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity(name="Category")
+@Table(name="CATEGORY")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Chores> choresList = new ArrayList<>();
+
+
 }
